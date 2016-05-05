eb7026574e082b80a7d0dd4b259c5fc347eeec7e
CMHung <cmhungsteve@gatech.edu>
2016-05-05 17:03:05 -0400

    update README for TCNN and other output files

--- TCNN/namelist.lua
+++ TCNN/namelist.lua
@@ -1,6 +1,12 @@
-predlabeltxt=torch.load('labels_tcnn.txt','ascii')
-nameucf = torch.load('../Dataset/NameList/name_UCF101_test_1.t7')
-for i = 1,3754 do
-    print(nameucf.path[i]..' '..predlabeltxt[i])
+--usage: th namelist.lua > labels_tcnn.txt
+
+predlabeltxt = torch.load('labels.txt','ascii')
+probtxt = torch.load('prob.txt','ascii')
+nameucf = torch.load('/media/cmhung/MyDisk/CMHung_FS/Big_and_Data/PhDResearch/Code/Dataset/Features/Res/name_UCF101_test_1.t7')
+for i = 1, #nameucf.path do
+	for j = 1,3 do
+	    -- print(nameucf.path[i]..' '..probtxt[i][j])
+    print(nameucf.path[i]..' '..predlabeltxt[i][j]..' '..probtxt[i][j])
+	end
 end
 
