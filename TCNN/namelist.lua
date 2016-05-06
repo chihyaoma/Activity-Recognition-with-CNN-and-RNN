d7ecc9e503db33e570380b9ff7a3a254768a1a4a
cmhungsteve <vitec6@gmail.com>
2016-05-05 23:52:09 -0400

    add labels and probabilities for demo

--- TCNN/namelist.lua
+++ TCNN/namelist.lua
@@ -2,7 +2,7 @@
 
 predlabeltxt = torch.load('labels.txt','ascii')
 probtxt = torch.load('prob.txt','ascii')
-nameucf = torch.load('/media/cmhung/MyDisk/CMHung_FS/Big_and_Data/PhDResearch/Code/Dataset/Features/Res/name_UCF101_test_1.t7')
+nameucf = torch.load('/media/cmhung/MyDisk/CMHung_FS/Big_and_Data/PhDResearch/Code/Dataset/Features/NameList/name_UCF101_test_1.t7')
 for i = 1, #nameucf.path do
 	for j = 1,3 do
 	    -- print(nameucf.path[i]..' '..probtxt[i][j])
