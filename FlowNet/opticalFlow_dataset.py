3c079c6480c6fcc7f59743f6b2614829a9d2a424
chihyaoma <cyma@gatech.edu>
2016-05-16 15:39:14 -0400

    check if video has been processed then skip

--- FlowNet/opticalFlow_dataset.py
+++ FlowNet/opticalFlow_dataset.py
@@ -33,6 +33,9 @@ numClassTotal = len(nameClass)  # 101 classes
 # fourcc = cv2.cv.CV_FOURCC('X', 'V', 'I', 'D') # opencv 2.4
 fourcc = cv2.VideoWriter_fourcc(*'XVID')  # opencv 3.0
 
+# initialize the display window
+cv2.namedWindow('Previous, current frames and flow map')
+
 for c in range(numClassTotal):  # c = 0 ~ 100
     dirClass = dirDatabase + nameClass[c] + '/'
     nameSubVideo = os.listdir(dirClass)
@@ -63,85 +66,94 @@ for c in range(numClassTotal):  # c = 0 ~ 100
         nameParse = videoName.split(".")
         nameOutput = nameParse[0] + '_flow.' + nameParse[1]
 
-        out = cv2.VideoWriter(outdir + nameOutput, fourcc, Fr, (Wd, Ht))
+        # check if the output file existed
+        filename = outdir + nameOutput
 
-        # read the first frame
-        ret, prvs = cap.read()
-        # prvs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)  # convert to gray
-        # scale
+        if not os.path.exists(filename):
+            
+            out = cv2.VideoWriter(filename, fourcc, Fr, (Wd, Ht))
 
-        # save in HSV (because of the optical flow algorithm we used)
-        hsv = np.zeros_like(prvs)
-        hsv[..., 1] = 255
+            # read the first frame
+            ret, prvs = cap.read()
+            # prvs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)  # convert to gray
+            # scale
 
-        indFrame = 1
+            # save in HSV (because of the optical flow algorithm we used)
+            hsv = np.zeros_like(prvs)
+            hsv[..., 1] = 255
 
-        while(cap.isOpened):
-            # Capture frame-by-frame
-            ret, next = cap.read()
+            indFrame = 1
 
-            if (indFrame % 7) == 0:
-                if ret is True:
+            while(cap.isOpened):
+                # Capture frame-by-frame
+                ret, next = cap.read()
 
-                    # Get frame sizes
-                    height, width, channels = prvs.shape
+                if (indFrame % 7) == 0:
+                    if ret is True:
 
-                    imgDisplay = np.hstack((prvs, next))
+                        # Get frame sizes
+                        height, width, channels = prvs.shape
 
-                    # save the frames into png files for FlowNet to read
-                    # TODO: stupid but is the easiest way without reconfigure
-                    # the FlowNet and possible re-train the model
-                    cv2.imwrite('data/frame1.png', prvs)
-                    cv2.imwrite('data/frame2.png', next)
+                        imgDisplay = np.hstack((prvs, next))
 
-                    # compute the optical flow from two adjacent frames
-                    FlowNet.run(prvs)  # the FlowNet will save a .flo file
+                        # save the frames into png files for FlowNet to read
+                        # TODO: stupid but is the easiest way without reconfigure
+                        # the FlowNet and possible re-train the model
+                        cv2.imwrite('data/frame1.png', prvs)
+                        cv2.imwrite('data/frame2.png', next)
 
-                    # read the .flo file
-                    fileName = 'flownetc-pred-0000000.flo'
-                    flowMapSize = np.fromfile(fileName, np.float32, count=1)
-                    if flowMapSize != 202021.25:
-                        print 'Dimension incorrect. Invalid .flo file'
+                        # compute the optical flow from two adjacent frames
+                        FlowNet.run(prvs)  # the FlowNet will save a .flo file
+
+                        # read the .flo file
+                        fileName = 'flownetc-pred-0000000.flo'
+                        flowMapSize = np.fromfile(fileName, np.float32, count=1)
+                        if flowMapSize != 202021.25:
+                            print 'Dimension incorrect. Invalid .flo file'
+                        else:
+                            data = np.fromfile(fileName, np.float32,
+                                               count=2 * width * height)
+
+                        flow = np.resize(data, (height, width, 2))
+
+                        for index, x in np.ndenumerate(flow):
+                            if x > 100:
+                                flow[index] = 0
+
+                        # compute the optical flow from two adjacent frames
+                        # next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+                        # flow = cv2.calcOpticalFlowFarneback(
+                        #   prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+
+                        # show in RGB for visualization
+                        mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+                        hsv[..., 0] = ang * 180 / np.pi / 2
+                        hsv[..., 2] = cv2.normalize(
+                            mag, None, 0, 255, cv2.NORM_MINMAX)
+                        frameProc = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+                        out.write(frameProc)
+
+                        # Display the resulting frame
+                        imgDisplay = np.hstack((imgDisplay, frameProc))
+                        # cv2.imshow(
+                        #     'Previous, current frames and flow map for video: ' + nameParse[0], imgDisplay)
+
+                        cv2.imshow('Previous, current frames and flow map', imgDisplay)
+                        
+
+                        if cv2.waitKey(1) & 0xFF == ord('q'):
+                            break
+
+                        prvs = next
+
                     else:
-                        data = np.fromfile(fileName, np.float32,
-                                           count=2 * width * height)
-
-                    flow = np.resize(data, (height, width, 2))
-
-                    for index, x in np.ndenumerate(flow):
-                        if x > 100:
-                            flow[index] = 0
-
-                    # compute the optical flow from two adjacent frames
-                    # next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-                    # flow = cv2.calcOpticalFlowFarneback(
-                    #   prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-
-                    # show in RGB for visualization
-                    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-                    hsv[..., 0] = ang * 180 / np.pi / 2
-                    hsv[..., 2] = cv2.normalize(
-                        mag, None, 0, 255, cv2.NORM_MINMAX)
-                    frameProc = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-                    out.write(frameProc)
-
-                    # Display the resulting frame
-                    imgDisplay = np.hstack((imgDisplay, frameProc))
-                    cv2.imshow(
-                        'Previous, current frames and flow map for video: ' + nameParse[0], imgDisplay)
-
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
 
-                    prvs = next
+                indFrame = indFrame + 1
 
-                else:
-                    break
-
-            indFrame = indFrame + 1
-
-        # When everything done, release the capture
-        cap.release()
-        out.release()
-        cv2.destroyAllWindows()
+            # When everything done, release the capture
+            cap.release()
+            out.release()
+        
+cv2.destroyAllWindows()
