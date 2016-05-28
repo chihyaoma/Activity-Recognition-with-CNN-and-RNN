9d6fe9ab790299c5b0ab99d8794abd33721e7c25
Chih-Yao Ma <cyma@gatech.edu>
2016-05-28 12:26:17 -0400

    minor update main and opts

--- FlowCNN/opts.lua
+++ FlowCNN/opts.lua
@@ -7,6 +7,19 @@
 -- 
 -- 
 -- Contact: Chih-Yao Ma at <cyma@gatech.edu>
+
+--  ************************************************************************
+--  This code incorporates material from:
+
+--  fb.resnet.torch (https://github.com/facebook/fb.resnet.torch)
+--  Copyright (c) 2016, Facebook, Inc.
+--  All rights reserved.
+--
+--  This source code is licensed under the BSD-style license found in the
+--  LICENSE file in the root directory of this source tree. An additional grant
+--  of patent rights can be found in the PATENTS file in the same directory.
+--
+--  ************************************************************************
 ----------------------------------------------------------------
 local M = { }
 
