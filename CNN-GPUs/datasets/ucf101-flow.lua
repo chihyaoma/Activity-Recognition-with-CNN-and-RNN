a4a2bfc6d12c550d5e8b817dbaede47b33460b2c
chihyaoma <cyma@gatech.edu>
2016-06-12 17:38:56 -0400

    add dataloader for UCF-101 flow maps

---
+++ CNN-GPUs/datasets/ucf101-flow.lua
@@ -0,0 +1,106 @@
+---------------------------------------------------------------
+--  Activity-Recognition-with-CNN-and-RNN
+--  https://github.com/chihyaoma/Activity-Recognition-with-CNN-and-RNN
+-- 
+-- 
+--  Train a CNN on flow map of UCF-101 dataset 
+-- 
+-- 
+--  Contact: Chih-Yao Ma at <cyma@gatech.edu>
+---------------------------------------------------------------
+--
+--  Copyright (c) 2016, Facebook, Inc.
+--  All rights reserved.
+--
+--  This source code is licensed under the BSD-style license found in the
+--  LICENSE file in the root directory of this source tree. An additional grant
+--  of patent rights can be found in the PATENTS file in the same directory.
+--
+--  UCF-101 flow maps dataset loader
+--
+
+local image = require 'image'
+local paths = require 'paths'
+local t = require 'datasets/transforms'
+local ffi = require 'ffi'
+
+local M = {}
+local UCF101Dataset = torch.class('resnet.UCF101Dataset', M)
+
+function UCF101Dataset:__init(imageInfo, opt, split)
+   self.imageInfo = imageInfo[split]
+   self.opt = opt
+   self.split = split
+   self.dir = paths.concat(opt.data, split)
+   assert(paths.dirp(self.dir), 'directory does not exist: ' .. self.dir)
+end
+
+function UCF101Dataset:get(i)
+   local path = ffi.string(self.imageInfo.imagePath[i]:data())
+
+   local image = self:_loadImage(paths.concat(self.dir, path))
+   local class = self.imageInfo.imageClass[i]
+
+   return {
+      input = image,
+      target = class,
+   }
+end
+
+function UCF101Dataset:_loadImage(path)
+   local ok, input = pcall(function()
+      return image.load(path, 3, 'float')
+   end)
+
+   -- Sometimes image.load fails because the file extension does not match the
+   -- image format. In that case, use image.decompress on a ByteTensor.
+   if not ok then
+      local f = io.open(path, 'r')
+      assert(f, 'Error reading: ' .. tostring(path))
+      local data = f:read('*a')
+      f:close()
+
+      local b = torch.ByteTensor(string.len(data))
+      ffi.copy(b:data(), data, b:size(1))
+
+      input = image.decompress(b, 3, 'float')
+   end
+
+   return input
+end
+
+function UCF101Dataset:size()
+   return self.imageInfo.imageClass:size(1)
+end
+
+-- Computed from random subset of UCF-101 training flow maps
+local meanstd = {
+   mean = { 0.877, 0.795, 0.881 },
+   std = { 0.087, 0.105, 0.091 },
+}
+
+function UCF101Dataset:preprocess()
+   if self.split == 'train' then
+      return t.Compose{
+         t.RandomSizedCrop(224),
+         t.ColorJitter({
+            brightness = 0.4,
+            contrast = 0.4,
+            saturation = 0.4,
+         }),
+         t.ColorNormalize(meanstd),
+         t.HorizontalFlip(0.5),
+      }
+   elseif self.split == 'val' then
+      local Crop = self.opt.tenCrop and t.TenCrop or t.CenterCrop
+      return t.Compose{
+         t.Scale(256),
+         t.ColorNormalize(meanstd),
+         Crop(224),
+      }
+   else
+      error('invalid split: ' .. self.split)
+   end
+end
+
+return M.UCF101Dataset
