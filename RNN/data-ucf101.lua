f2bdd413ac520d5c34e135f4d3964404f7e990c0
Chih-Yao Ma <cyma@gatech.edu>
2016-07-02 17:10:52 -0400

    ucf101 classes re-ordering. Log folder with different names

--- RNN/data-ucf101.lua
+++ RNN/data-ucf101.lua
@@ -1,41 +1,41 @@
 ----------------------------------------------------------------
--- Georgia Tech 2016 Spring
--- Deep Learning for Perception
--- Final Project: LRCN model for Video Classification
+--  Activity-Recognition-with-CNN-and-RNN
+--  https://github.com/chihyaoma/Activity-Recognition-with-CNN-and-RNN
 --
 -- 
--- This is a testing code for implementing the RNN model with LSTM 
--- written by Chih-Yao Ma. 
+--  This is a testing code for implementing the RNN model with LSTM 
+--  written by Chih-Yao Ma. 
 -- 
--- The code will take feature vectors (from CNN model) from contiguous 
--- frames and train against the ground truth, i.e. the labeling of video classes. 
+--  The code will take feature vectors (from CNN model) from contiguous 
+--  frames and train against the ground truth, i.e. the labeling of video classes. 
 -- 
--- Contact: Chih-Yao Ma at <cyma@gatech.edu>
+--  Contact: Chih-Yao Ma at <cyma@gatech.edu>
 ----------------------------------------------------------------
 require 'torch'
 require 'sys'
--- nClass = 11 -- UCF11 has 11 categories
+
 nClass = 101 -- UCF101 has 101 categories
 
--- generate strings for classes
--- classes = {}
--- for c = 1, nClass do
---    classes[c] = tostring(c)
--- end
-
 -- classes in UCF-101
+
 classes = {
-"BoxingSpeedBag", "Surfing", "FloorGymnastics", "IceDancing", "Lunges", "Swing", "SkyDiving", "MilitaryParade", "PlayingPiano", "Punch",
-"HulaHoop", "VolleyballSpiking", "Skijet", "JavelinThrow", "LongJump", "Mixing", "Shotput", "BandMarching", "Kayaking", "StillRings",
-"PushUps", "Archery", "FieldHockeyPenalty", "BoxingPunchingBag", "PlayingCello", "FrontCrawl", "Billiards", "Rowing", "ApplyLipstick", "TrampolineJumping",
-"CuttingInKitchen", "BodyWeightSquats", "JugglingBalls", "Nunchucks", "JumpRope", "PlayingViolin", "PlayingGuitar", "YoYo", "SumoWrestling", "SoccerJuggling",
-"CliffDiving", "CricketBowling", "PlayingDhol", "HorseRiding", "BabyCrawling", "PlayingSitar", "TaiChi", "BenchPress", "PommelHorse", "BrushingTeeth",
-"Hammering", "PlayingTabla", "HandstandWalking", "Typing", "CleanAndJerk", "TennisSwing", "CricketShot", "BlowDryHair", "HeadMassage", "BalanceBeam",
-"TableTennisShot", "MoppingFloor", "Drumming", "PlayingFlute", "FrisbeeCatch", "ApplyEyeMakeup", "SkateBoarding", "BaseballPitch", "SoccerPenalty", "ThrowDiscus",
-"RopeClimbing", "HorseRace", "HighJump", "PullUps", "Diving", "BreastStroke", "ParallelBars", "WalkingWithDog", "PizzaTossing", "BlowingCandles",
-"GolfSwing", "PoleVault", "UnevenBars", "HandstandPushups", "JumpingJack", "WallPushups", "WritingOnBoard", "Skiing", "Bowling", "BasketballDunk",
-"SalsaSpin", "ShavingBeard", "Basketball", "Knitting", "RockClimbingIndoor", "Haircut", "Biking", "Fencing", "Rafting", "PlayingDaf",
-"HammerThrow"
+"ApplyEyeMakeup", "ApplyLipstick", "Archery", "BabyCrawling", "BalanceBeam", "BandMarching",
+"BaseballPitch", "Basketball", "BasketballDunk", "BenchPress", "Biking", "Billiards",
+"BlowDryHair", "BlowingCandles", "BodyWeightSquats", "Bowling", "BoxingPunchingBag",
+"BoxingSpeedBag", "BreastStroke", "BrushingTeeth", "CleanAndJerk", "CliffDiving",
+"CricketBowling", "CricketShot", "CuttingInKitchen", "Diving", "Drumming", "Fencing",
+"FieldHockeyPenalty", "FloorGymnastics", "FrisbeeCatch", "FrontCrawl", "GolfSwing",
+"Haircut", "HammerThrow", "Hammering", "HandstandPushups", "HandstandWalking",
+"HeadMassage", "HighJump", "HorseRace", "HorseRiding", "HulaHoop", "IceDancing",
+"JavelinThrow", "JugglingBalls", "JumpRope", "JumpingJack", "Kayaking", "Knitting",
+"LongJump", "Lunges", "MilitaryParade", "Mixing", "MoppingFloor", "Nunchucks", "ParallelBars",
+"PizzaTossing", "PlayingCello", "PlayingDaf", "PlayingDhol", "PlayingFlute", "PlayingGuitar",
+"PlayingPiano", "PlayingSitar", "PlayingTabla", "PlayingViolin", "PoleVault", "PommelHorse",
+"PullUps", "Punch", "PushUps", "Rafting", "RockClimbingIndoor", "RopeClimbing", "Rowing",
+"SalsaSpin", "ShavingBeard", "Shotput", "SkateBoarding", "Skiing", "Skijet", "SkyDiving",
+"SoccerJuggling", "SoccerPenalty", "StillRings", "SumoWrestling", "Surfing", "Swing",
+"TableTennisShot", "TaiChi", "TennisSwing", "ThrowDiscus", "TrampolineJumping", "Typing",
+"UnevenBars", "VolleyballSpiking", "WalkingWithDog", "WallPushups", "WritingOnBoard", "YoYo"
 }
 
 ------------------------------------------------------------
@@ -100,84 +100,30 @@ end
 
 print(sys.COLORS.green .. '==> Reading UCF101 external feature vector and target file ...')
 -- load saved feature matrix from CNN model
-local UCF101_list = true
 
-if UCF101_list == true then 
+-- training and testing data from UCF101 website
+assert(paths.filep(paths.concat(opt.featFile, 'data_UCF101_train_1.t7')), 'no training feature file found.')
+local TrainFeatureLabels = torch.load(paths.concat(opt.featFile, 'data_UCF101_train_1.t7'))
+TrainData = TrainFeatureLabels.featMats
 
-   -- training and testing data from UCF101 website
-   local TrainFeatureLabels = torch.load('/home/chih-yao/Documents/CNN-Temporal/data_UCF101_train_1.t7')
-   TrainData = TrainFeatureLabels.featMats
+-- check if there are enough frames to extract
+assert(TrainData:size(3) >= opt.rho, '# of frames lower than rho')
 
+-- extract #rho of frames
+TrainData = ExtractFrames(TrainData, opt.rho)
+TrainTarget = TrainFeatureLabels.labels
+
+assert(paths.filep(paths.concat(opt.featFile, 'data_UCF101_test_1.t7')), 'no testing feature file found.')
+local TestFeatureLabels = torch.load(paths.concat(opt.featFile, 'data_UCF101_test_1.t7'))
+TestData = TestFeatureLabels.featMats
+
+if opt.averagePred == false then 
    -- check if there are enough frames to extract
    assert(TrainData:size(3) >= opt.rho, '# of frames lower than rho')
-   
    -- extract #rho of frames
-   TrainData = ExtractFrames(TrainData, opt.rho)
-   TrainTarget = TrainFeatureLabels.labels
-
-   local TestFeatureLabels = torch.load('/home/chih-yao/Documents/CNN-Temporal/data_UCF101_test_1.t7')
-   TestData = TestFeatureLabels.featMats
-
-   if opt.AveragePred == false then 
-      -- check if there are enough frames to extract
-      assert(TrainData:size(3) >= opt.rho, '# of frames lower than rho')
-      -- extract #rho of frames
-      TestData = ExtractFrames(TestData, opt.rho)
-   end
-   TestTarget = TestFeatureLabels.labels
-
-else
-   print(sys.COLORS.green .. '==> Reading self-defined external feature vector and target file . . .')
-   
-   ds = {}
-
-   local InputFeatureLabels = torch.load('/home/chih-yao/Downloads/feat_label_UCF101_2.t7')
-
-   ds.input = InputFeatureLabels.featMats
-   ds.target = InputFeatureLabels.labels
-   ds.size = ds.input:size(1)
-   ds.FeatureDims = ds.input:size(2)
-
-   if not InputFeatureLabels then error("Cannot read feature vector file") end
-
-   -- Only use a certain number of (consecutive) frames from each video
-   ds.input = ExtractFrames(ds.input, opt.rho)
-   -- ds.input = ExtractConsecutiveFrames(ds.input, opt.rho)
-
-   -- n-fold cross-validation
-   TrainData, TrainTarget, TestData, TestTarget = CrossValidation(ds.input, ds.target, 5)
-
+   TestData = ExtractFrames(TestData, opt.rho)
 end
-
-
-
-
-
-if (false) then
--- input dimension = ds.size x ds.FeatureDims x opt.rho = 1100 x 1024 x time
-   if not (opt.featFile == '') then
-      -- read feature file from command line
-      print(' - - Reading external feature file . . .')
-      file = torch.DiskFile(opt.featFile, 'r')
-      ds.input = file:readObject()
-   else
-      -- generate random feature file
-      print(sys.COLORS.red .. ' - - No --featFile specified. Generating random feature matrix . . . ' .. c.white)
-      ds.input = torch.randn(ds.input:size(1), ds.FeatureDims, opt.rho)
-   end
-
-   -- target dimension = ds.size x 1 = 1100 x 1
-   if not (opt.targFile == '') then
-      -- read feature file from command line
-      print(' - - Reading external target file . . .')
-      file = torch.DiskFile(opt.targFile, 'r')
-      ds.target = file:readObject()
-   else
-      print(sys.COLORS.red .. ' - - No --targFile specified. Generating random target vector . . . ' .. c.white)
-      ds.target = torch.DoubleTensor(ds.input:size(1)):random(nClass)
-   end
-end
-
+TestTarget = TestFeatureLabels.labels
 
 return 
 {
--- RNN/main.lua
+++ RNN/main.lua
@@ -3,39 +3,37 @@
 --  https://github.com/chihyaoma/Activity-Recognition-with-CNN-and-RNN
 --
 -- 
--- This is a testing code for implementing the RNN model with LSTM 
--- written by Chih-Yao Ma. 
+--  This is a testing code for implementing the RNN model with LSTM 
+--  written by Chih-Yao Ma. 
 -- 
--- The code will take feature vectors (from CNN model) from contiguous 
--- frames and train against the ground truth, i.e. the labeling of video classes. 
+--  The code will take feature vectors (from CNN model) from contiguous 
+--  frames and train against the ground truth, i.e. the labeling of video classes. 
 -- 
--- Contact: Chih-Yao Ma at <cyma@gatech.edu>
+--  Contact: Chih-Yao Ma at <cyma@gatech.edu>
 ----------------------------------------------------------------
 
 require 'dp'
 require 'sys'
-
-version = 1
 
 --[[command line arguments]]--
 cmd = torch.CmdLine()
 cmd:text()
 cmd:text('Train a Model on video classification dataset using RNN with LSTM or GRU')
 cmd:text('Example:')
-cmd:text("RNN_LSTM.lua --cuda --useDevice 1 --progress --opt.rho 36")
+cmd:text("main.lua --cuda --useDevice 1 --progress --opt.rho 36")
 cmd:text('Options:')
-cmd:option('--learningRate', 1e-3, 'learning rate at t=0')
-cmd:option('--minLR', 0.00001, 'minimum learning rate')
+cmd:option('--learningRate', 5e-5, 'learning rate at t=0')
+cmd:option('--minLR', 5e-7, 'minimum learning rate')
 cmd:option('--learningRateDecay', 1e-7, 'learningRateDecay')
 cmd:option('--saturateEpoch', 400, 'epoch at which linear decayed LR will reach minLR')
 cmd:option('--momentum', 0.9, 'momentum')
 cmd:option('--weightDecay', 1e-5, 'weightDecay')
 cmd:option('--optimizer', 'adam', 'Use different optimizer, e.g. sgd, adam, adamax, rmsprop for now')
 cmd:option('--lrDecayEvery', 5, 'learning rate decay per epochs for optimizer adam')
-cmd:option('--lrDecayFactor', 0.8, 'learning rate decay factor for optimizer adam')
+cmd:option('--lrDecayFactor', 0.75, 'learning rate decay factor for optimizer adam')
 cmd:option('--maxOutNorm', -1, 'max l2-norm of each layer\'s output neuron weights')
 cmd:option('--cutoffNorm', -1, 'max l2-norm of concatenation of all gradParam tensors')
-cmd:option('--batchSize', 512, 'number of examples per batch') -- how many examples per training 
+cmd:option('--batchSize', 128, 'number of examples per batch') -- how many examples per training 
 cmd:option('--cuda', true, 'use CUDA')
 cmd:option('--useDevice', 1, 'sets the device (GPU) to use')
 cmd:option('--maxEpoch', 1000, 'maximum number of epochs to run')
@@ -47,35 +45,31 @@ cmd:option('--uniform', 0.1, 'initialize parameters using uniform distribution b
 -- recurrent layer 
 cmd:option('--lstm', true, 'use Long Short Term Memory (nn.LSTM instead of nn.Recurrent)')
 cmd:option('--gru', false, 'use Gated Recurrent Units (nn.GRU instead of nn.Recurrent)')
-cmd:option('--rho', 16, 'number of frames for each video')
-cmd:option('--inputSize', 512, 'dimension of the feature vector from CNN')
-cmd:option('--hiddenSize', '{512, 256}', 'number of hidden units used at output of each recurrent layer. When more than one is specified, RNN/LSTMs/GRUs are stacked')
+cmd:option('--rho', 48, 'number of frames for each video')
+cmd:option('--inputSize', 2048, 'dimension of the feature vector from CNN')
+cmd:option('--hiddenSize', '{2048, 1024, 512}', 'number of hidden units used at output of each recurrent layer. When more than one is specified, RNN/LSTMs/GRUs are stacked')
 cmd:option('--zeroFirst', false, 'first step will forward zero through recurrence (i.e. add bias of recurrence). As opposed to learning bias specifically for first step.')
 cmd:option('--dropout', true, 'apply dropout after each recurrent layer')
 cmd:option('--dropoutProb', 0.5, 'probability of zeroing a neuron (dropout probability)')
 
 -- testing process
-cmd:option('--AveragePred', true, 'average the predictions from each time step per video')
+cmd:option('--averagePred', true, 'average the predictions from each time step per video')
 
 -- data
 cmd:option('--trainEpochSize', -1, 'number of train examples seen between each epoch')
 cmd:option('--validEpochSize', -1, 'number of valid examples used for early stopping and cross-validation') 
+cmd:option('--featFile', '/home/chih-yao/Documents/CNN-Spatial/', 'directory of feature vectors')
 
-cmd:option('--featFile', '', 'file of feature vectors')
-cmd:option('--targFile', '', 'file of target vector')
 dname,fname = sys.fpath()
--- cmd:option('-save', fname:gsub('.lua',''), 'subdirectory to save/log experiments in')
-
-cmd:option('-save', 'log', 'subdirectory to save/log experiments in')
 cmd:option('--plot', true, 'Plot the training and testing accuracy')
 
 cmd:text()
 opt = cmd:parse(arg or {})
-opt.rundir = 'log'
-paths.mkdir(opt.rundir)
+opt.save = 'log' .. '_' .. opt.hiddenSize .. '_' .. opt.learningRate
+paths.mkdir(opt.save)
 
 -- create log file
-cmd:log(opt.rundir .. '/log.txt', opt)
+cmd:log(opt.save .. '/log.txt', opt)
 
 opt.hiddenSize = dp.returnString(opt.hiddenSize)
 if not opt.silent then
@@ -93,7 +87,7 @@ end
 ------------------------------------------------------------
 print(sys.COLORS.red ..  '==> load modules')
 
-local data  = require 'data'
+local data  = require 'data-ucf101'
 local train = require 'train'
 local test  = require 'test'
 
@@ -105,22 +99,12 @@ bestAcc = 0
 
 print(sys.COLORS.red .. '==> training!')
 
-
 for iteration = 1, opt.maxEpoch do
 
    -- Begin training process
    train(data.TrainData, data.TrainTarget)
 
    -- Begin testing with trained model
-	test(data.TestData, data.TestTarget)
+   test(data.TestData, data.TestTarget)
 
 end
-
---[[
-local answer
-repeat
-   io.write("continue with this operation (y/n)? ")
-   io.flush()
-   answer=io.read()
-until answer=="y" or answer=="n"
---]]
