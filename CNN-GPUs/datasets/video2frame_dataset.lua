d2c694cbfe333ab904494b60dda291de3f89d39c
cmhungsteve <vitec6@gmail.com>
2016-10-31 13:58:38 -0400

    generate frames for HMDB-51

--- CNN-GPUs/datasets/video2frame_dataset.lua
+++ CNN-GPUs/datasets/video2frame_dataset.lua
@@ -19,28 +19,72 @@ require 'torch'
 -- require 'imgraph'
 -- require 'nnx'
 require 'ffmpeg'
+require 'torch-ffmpeg'
 require 'image'
 require 'cutorch'
+
+----------------
+-- parse args --
+----------------
+op = xlua.OptionParser('%prog [options]')
+
+op:option{'-sP', '--sourcePath', action='store', dest='sourcePath',
+          help='source path (local | workstation)', default='local'}
+op:option{'-dB', '--nameDatabase', action='store', dest='nameDatabase',
+          help='used database (UCF-101 | HMDB-51)', default='HMDB-51'}
+op:option{'-sT', '--stream', action='store', dest='stream',
+          help='type of stream (FlowMap-Brox | FlowMap-TVL1-crop20 | RGB)', default='RGB'}
+op:option{'-iSp', '--idSplit', action='store', dest='idSplit',
+          help='index of the split set', default=1}
+op:option{'-f', '--fps', action='store', dest='fps',
+          help='number of frames per second', default=30}
+op:option{'-i', '--devid', action='store', dest='devid',
+          help='device ID (if using CUDA)', default=1}
+op:option{'-s', '--save', action='store', dest='save',
+          help='save the intermediate data or not', default=false}
+
+op:option{'-t', '--time', action='store', dest='seconds',
+          help='length to process (in seconds)', default=2}
+op:option{'-w', '--width', action='store', dest='width',
+          help='resize video, width', default=320}
+op:option{'-h', '--height', action='store', dest='height',
+          help='resize video, height', default=240}
+
+opt,args = op:parse()
+-- convert strings to numbers --
+idSplit = tonumber(opt.idSplit)
+fps = tonumber(opt.fps)
+devid = tonumber(opt.devid)
+
+print('Split #: '..idSplit)
+print('source path: '..opt.sourcePath)
+print('fps: '..fps)
+
+----------------------------------------------
+-- 			User-defined parameters			--
+----------------------------------------------
+numSplit = 3
 
 ----------------------------------------------
 -- 				Data paths				    --
 ----------------------------------------------
-source = 'local' -- local | workstation
+source = opt.sourcePath -- local | workstation
 if source == 'local' then
 	dirSource = '/home/cmhung/Code/'
 elseif source == 'workstation' then	
 	dirSource = '/home/chih-yao/Downloads/'
 end
 
-nameDatabase = 'UCF-101'
+nameDatabase = opt.nameDatabase
 pathDatabase = dirSource..'dataset/'..nameDatabase..'/'
 
 -- input
-dirVideoIn = 'RGB' -- FlowMap-Brox | FlowMap-TVL1-crop20 | RGB
+dirVideoIn = opt.stream 
 pathVideoIn = pathDatabase .. dirVideoIn .. '/'
+pathTxtSplit = pathDatabase .. 'testTrainMulti_7030_splits/' -- for HMDB-51
 
 -- outputs
-dirVideoOut = dirVideoIn .. '-frame'
+dirVideoOut = dirVideoIn .. '-frame-sp' .. idSplit
 dirTrain = 'train' 
 dirTest = 'val' 
 pathTrain = pathDatabase .. dirVideoOut .. '/' .. dirTrain .. '/'
@@ -57,32 +101,10 @@ end
 
 -- Output information --
 outTrain = {}
-table.insert(outTrain, {name = 'data_'..nameDatabase..'_train_'..dirVideoIn..'.t7'})
+table.insert(outTrain, {name = 'data_'..nameDatabase..'_train_'..dirVideoIn..'_sp'..idSplit..'.t7'})
 
 outTest = {}
-table.insert(outTest, {name = 'data_'..nameDatabase..'_val_'..dirVideoIn..'.t7'})
-
-----------------
--- parse args --
-----------------
-op = xlua.OptionParser('%prog [options]')
-op:option{'-f', '--fps', action='store', dest='fps',
-          help='number of frames per second', default=25}
-op:option{'-t', '--time', action='store', dest='seconds',
-          help='length to process (in seconds)', default=2}
-op:option{'-w', '--width', action='store', dest='width',
-          help='resize video, width', default=320}
-op:option{'-h', '--height', action='store', dest='height',
-          help='resize video, height', default=240}
-op:option{'-z', '--zoom', action='store', dest='zoom',
-          help='display zoom', default=1}
-op:option{'-m', '--mode', action='store', dest='mode',
-          help='option for generating features (pred|feat)', default='pred'}
-op:option{'-p', '--type', action='store', dest='type',
-          help='option for CPU/GPU', default='cuda'}
-op:option{'-i', '--devid', action='store', dest='devid',
-          help='device ID (if using CUDA)', default=1}      
-opt,args = op:parse()
+table.insert(outTest, {name = 'data_'..nameDatabase..'_val_'..dirVideoIn..'_sp'..idSplit..'.t7'})
 
 ----------------------------------------------
 --  		       GPU option	 	        --
@@ -98,34 +120,30 @@ print(sys.COLORS.white ..  ' ')
 -- numClass = 101
 -- dimFeat = 2048
 
-----------------------------------------------
--- 			User-defined parameters			--
-----------------------------------------------
-numSplit = 3
-idSplit = 1
-saveData = true
-
 -- Train/Test split
-groupSplit = {}
-for sp=1,numSplit do
-	if sp==1 then
-		table.insert(groupSplit, {setTr = torch.Tensor({{8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25}}),
-			setTe = torch.Tensor({{1,2,3,4,5,6,7}})})
-	elseif sp==2 then
-		table.insert(groupSplit, {setTr = torch.Tensor({{1,2,3,4,5,6,7,15,16,17,18,19,20,21,22,23,24,25}}),
-			setTe = torch.Tensor({{8,9,10,11,12,13,14}})})
-	elseif sp==3 then
-		table.insert(groupSplit, {setTr = torch.Tensor({{1,2,3,4,5,6,7,8,9,10,11,12,13,14,22,23,24,25}}),
-			setTe = torch.Tensor({{15,16,17,18,19,20,21}})})
+if nameDatabase == 'UCF-101' then
+	groupSplit = {}
+	for sp=1,numSplit do
+		if sp==1 then
+			table.insert(groupSplit, {setTr = torch.Tensor({{8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25}}),
+				setTe = torch.Tensor({{1,2,3,4,5,6,7}})})
+		elseif sp==2 then
+			table.insert(groupSplit, {setTr = torch.Tensor({{1,2,3,4,5,6,7,15,16,17,18,19,20,21,22,23,24,25}}),
+				setTe = torch.Tensor({{8,9,10,11,12,13,14}})})
+		elseif sp==3 then
+			table.insert(groupSplit, {setTr = torch.Tensor({{1,2,3,4,5,6,7,8,9,10,11,12,13,14,22,23,24,25}}),
+				setTe = torch.Tensor({{15,16,17,18,19,20,21}})})
+		end
 	end
 end
-
 ----------------------------------------------
 -- 					Class		        	--
 ----------------------------------------------
 nameClass = paths.dir(pathVideoIn) 
 table.sort(nameClass) -- ascending order
-numClassTotal = #nameClass -- 101 classes + "." + ".."
+table.remove(nameClass,1) -- remove "."
+table.remove(nameClass,1) -- remove ".."
+numClassTotal = #nameClass -- 101 classes
 
 --====================================================================--
 --                     Run all the videos in UCF-101                  --
@@ -134,7 +152,7 @@ print '==> Processing all the videos...'
 
 -- Load the intermediate data or generate a new one --
 -- Training data --
-if not (saveData and paths.filep(outTrain[idSplit].name)) then
+if not (opt.save and paths.filep(outTrain[idSplit].name)) then
 	Tr = {} -- output
 	Tr.name = {}
 	Tr.path = {}
@@ -146,7 +164,7 @@ else
 end
 
 -- Testing data --
-if not (saveData and paths.filep(outTest[idSplit].name)) then
+if not (opt.save and paths.filep(outTest[idSplit].name)) then
 	Te = {} -- output
 	Te.name = {}
 	Te.path = {}
@@ -164,14 +182,27 @@ if Tr.c_finished == numClassTotal and Te.c_finished == numClassTotal then
 	print('The feature data of split '..idSplit..' is already in your folder!!!!!!')
 else
 	for c=Tr.c_finished+1, numClassTotal do
-		if nameClass[c] ~= '.' and nameClass[c] ~= '..' then
+		
 			print('Current Class: '..c..'. '..nameClass[c])
 			Tr.countClass = Tr.countClass + 1
+			Te.countClass = Te.countClass + 1
+			countVideoClassTr = 0
+			countVideoClassTe = 0
 		  	------ Data paths ------
 		  	-- input
 		  	local pathClassIn = pathVideoIn .. nameClass[c] .. '/' 
-		  	local nameSubVideo = paths.dir(pathClassIn)
-		  	table.sort(nameSubVideo) -- ascending order
+
+		  	local nameSubVideo = {}
+		  	if nameDatabase == 'UCF-101' then
+		  		nameSubVideo = paths.dir(pathClassIn)
+			  	table.sort(nameSubVideo) -- ascending order
+		  	elseif nameDatabase == 'HMDB-51' then
+		  		local nameTxt = pathTxtSplit..nameClass[c]..'_test_split'..idSplit..'.txt'
+		  		for l in io.lines(nameTxt) do
+					table.insert(nameSubVideo,l)
+				end
+		  	end
+		  	
 		  	local numSubVideoTotal = #nameSubVideo -- videos + '.' + '..'
 		  	
 		  	-- outputs
@@ -194,13 +225,29 @@ else
 			       	--------------------
 			    	-- Load the video --
 			    	--------------------  
-			       	local videoName = paths.basename(nameSubVideo[sv],'avi')
-			       	local videoPath = pathClassIn..videoName..'.avi'
+			    	local videoName
+			    	local videoIndex -- for HMDB-51
+			       	if nameDatabase == 'UCF-101' then
+			       		videoName = paths.basename(nameSubVideo[sv],'avi')
+			       	elseif nameDatabase == 'HMDB-51' then
+			       		local i,j = string.find(nameSubVideo[sv],' ') -- find the location of the group info in the string
+				    	videoName = string.sub(nameSubVideo[sv],1,j-5) -- get the video name
+						videoIndex = tonumber(string.sub(nameSubVideo[sv],j+1)) -- get the index for training/testing
+			       	end
+			       	local videoPath
+			       	if dirVideoIn == 'RGB' then
+				       	videoPath = pathClassIn..videoName..'.avi'
+				    else
+				    	videoPath = pathClassIn..videoName..'_flow.avi'
+				    end
+			       	
+
 			       	--
 			       	-- print('==> Current video: '..videoName)
 			    	
-			    	local video = ffmpeg.Video{path=videoPath, fps=opt.fps, delete=true, destFolder='out_frames',silent=true}
-			       	-- local video = ffmpeg.Video{path=videoPath, delete=true, destFolder='out_frames', silent=true}
+			    	local vid = FFmpeg(videoPath)
+			    	local vidStats = vid:stats()
+			    	local video = ffmpeg.Video{path=videoPath, fps=opt.fps, width = vidStats.width, height = vidStats.height, delete=true, destFolder='out_frames',silent=true}
 			       	--
 			       	local vidTensor = video:totensor{} -- read the whole video & turn it into a 4D tensor
 
@@ -214,21 +261,45 @@ else
 				    -- Train/Test split --
 				    ----------------------
 				    -- find out whether the video is in training set or testing set
-				    local i,j = string.find(videoName,'_g') -- find the location of the group info in the string
-				    local videoGroup = tonumber(string.sub(videoName,j+1,j+2)) -- get the group#
-				    local videoPathLocal = nameClass[c] .. '/' .. videoName .. '.avi'
+				    local flagTrain, flagTest = false
+				    if nameDatabase == 'UCF-101' then
+					    local i,j = string.find(videoName,'_g') -- find the location of the group info in the string
+					    local videoGroup = tonumber(string.sub(videoName,j+1,j+2)) -- get the group#
+					    local videoPathLocal = nameClass[c] .. '/' .. videoName .. '.avi'
 
-				    if groupSplit[idSplit].setTe:eq(videoGroup):sum() == 0 then -- training data
-				    	Tr.countVideo = Tr.countVideo + 1
-				        Tr.name[Tr.countVideo] = videoName
-				        Tr.path[Tr.countVideo] = videoPathLocal
-				        pathClassOut = pathClassTrain
-				    else -- testing data
-				    	Te.countVideo = Te.countVideo + 1
-	            		Te.name[Te.countVideo] = videoName
-		            	Te.path[Te.countVideo] = videoPathLocal
-		            	pathClassOut = pathClassTest
-				    end
+					    if groupSplit[idSplit].setTe:eq(videoGroup):sum() == 0 then -- training data
+					    	flagTrain = true
+					    	flagTest = false
+					    else -- testing data
+					    	flagTrain = false
+					    	flagTest = true
+					    end
+					elseif nameDatabase == 'HMDB-51' then
+						if videoIndex == 1 then -- training data
+							flagTrain = true
+							flagTest = false
+						elseif videoIndex == 2 then -- testing data
+							flagTrain = false
+							flagTest = true
+						else
+							flagTrain = false
+							flagTest = false
+						end
+					end
+
+					if flagTrain and not flagTest then -- training data
+					  	Tr.countVideo = Tr.countVideo + 1
+					  	countVideoClassTr = countVideoClassTr + 1
+					    Tr.name[Tr.countVideo] = videoName
+					    Tr.path[Tr.countVideo] = videoPathLocal
+					    pathClassOut = pathClassTrain
+					elseif not flagTrain and flagTest then -- testing data
+					  	Te.countVideo = Te.countVideo + 1
+					  	countVideoClassTe = countVideoClassTe + 1
+		            	Te.name[Te.countVideo] = videoName
+			           	Te.path[Te.countVideo] = videoPathLocal
+			           	pathClassOut = pathClassTest
+			        end
 
 				    -- save all the frames
 					for f=1, numFrame do
@@ -247,18 +318,19 @@ else
 			Te.c_finished = c -- save the index
 			-- print('Split: '..idSplit)
 			-- print('Finished class#: '..Tr.countClass)
-			print('Generated training data#: '..Tr.countVideo)
-			print('Generated testing data#: '..Te.countVideo)
+			print('Generated training data# in this class: '..countVideoClassTr)
+			print('Generated testing data# in this class: '..countVideoClassTe)
+			print('Generated total training data#: '..Tr.countVideo)
+			print('Generated total testing data#: '..Te.countVideo)
 			print('The elapsed time for the class '..nameClass[c]..': ' .. timerClass:time().real .. ' seconds')
 			
-			if saveData then
+			if opt.save then
 				torch.save(outTrain[idSplit].name, Tr)
 				torch.save(outTest[idSplit].name, Te)
 			end
 
 			collectgarbage()
 			print(' ')
-		end
 		
 	end
 end       	
