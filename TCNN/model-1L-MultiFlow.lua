9cdd576692fa7de9c9345fe31bfded61014171e5
cmhungsteve <vitec6@gmail.com>
2017-01-17 12:19:39 -0500

    modify model-1L-MultiFlow

--- TCNN/model-1L-MultiFlow.lua
+++ TCNN/model-1L-MultiFlow.lua
@@ -30,7 +30,7 @@ print '==> processing options'
 print(sys.COLORS.red ..  '==> define parameters')
 
 -- 101 classes problem
-local noutputs = 101
+local noutputs = #data.classes
 
 -- input: 
 local frameSkip = 0
@@ -38,16 +38,17 @@ local nframeAll = data.trainData.data:size(3)
 local nframeUse = nframeAll - frameSkip
 local nfeature = data.trainData.data:size(2)
 local bSize = tonumber(opt.batchSize)
-local dropout = tonumber(opt.dropout)
+local dropout1 = tonumber(opt.dropout1)
+local dropout2 = tonumber(opt.dropout2)
 local dimMap = 1
 
 -- hidden units, filter sizes  	
 ---- 2-flow	
 local nstate_CNN = {4, 4} -- neuron # after pooling
-local nstate_FC = {1280, 1280} -- neuron # after 1st FC
-local nstate_FC_all = 1280 -- neuron # after the last FC
+local nstate_FC = {1024, 1024} -- neuron # after 1st FC
+local nstate_FC_all = 1024 -- neuron # after the last FC
 
-local convsize = {5,7}
+local convsize = {5,9}
 local convstep = {1,1}
 local convpad  = {(convsize[1]-1)/2,(convsize[2]-1)/2}
 --local poolsize = {3,4}
@@ -71,6 +72,46 @@ local numFlow = #convsize
 local model = nn.Sequential()
 local model_name = "model"
 
+-- ------------------------------------
+-- -- Full-connected layer for input --
+-- ------------------------------------
+-- local batch_FC = nn.Sequential()
+-- -- 1. dispart the mini-patch feature maps to lots of feature vectors
+-- batch_FC:add(nn.SplitTable(1)) -- split the mini-batches into single feature maps
+
+-- local vectorTable = nn.ParallelTable()
+-- for b=1,bSize do 
+--    vectorTable:add(nn.SplitTable(-1)) -- split one feature map into feature vectors
+-- end
+-- batch_FC:add(vectorTable)
+
+-- -- 2. duplicate thr fully-connected layer to fit the input
+-- -- FC layer
+-- local vectorFC = nn.Sequential()
+-- vectorFC:add(nn.Linear(nfeature,nfeature))
+-- -- if opt.batchNormalize == 'Yes' then vectorFC:add(nn.BatchNormalization(nfeature)) end
+-- -- vectorFC:add(nn.ReLU())
+-- -- vectorFC:add(nn.Dropout(0.5)) -- dropout
+
+-- local mapFC = nn.MapTable():add(nn.MapTable():add(vectorFC))
+-- batch_FC:add(mapFC)
+
+-- -- 3. convert the whole table back to the original mini-batch
+-- local combineTable = nn.ParallelTable()
+-- for b=1,bSize do 
+--    combineTable:add(nn.JoinTable(-1)) -- merge all the vectors back to map
+-- end
+-- batch_FC:add(combineTable)
+
+-- local viewTable = nn.ParallelTable()
+-- for b=1,bSize do 
+--    viewTable:add(nn.View(dimMap,nfeature,nframeUse)) -- (1,4096*25) --> (1,4096,25)
+-- end
+-- batch_FC:add(viewTable)
+
+-- batch_FC:add(nn.JoinTable(1)) -- merge all the maps back to mini-batch
+-- batch_FC:add(nn.View(bSize,dimMap,nfeature,nframeUse)) -- 32x1x4096x25
+
 -----------------------
 -- Main Architecture --
 -----------------------
@@ -90,19 +131,24 @@ if opt.model == 'model-1L-MultiFlow' then
 
       	local CNN_1L = nn.Sequential()
 
+         -- -- stage 0: mini-batch FC
+         -- CNN_1L:add(batch_FC)
+
       	-- stage 1: Conv -> ReLU -> Pooling
       	CNN_1L:add(nn.SpatialConvolutionMM(dimMap,nstate_CNN[n],convsize[n],1,convstep[n],1,convpad[n],0))
       	if opt.batchNormalize == 'Yes' then CNN_1L:add(nn.SpatialBatchNormalization(nstate_CNN[n])) end
       	CNN_1L:add(nn.ReLU())
+        --if opt.batchNormalize == 'Yes' then CNN_1L:add(nn.SpatialBatchNormalization(nstate_CNN[n])) end
       	CNN_1L:add(nn.SpatialMaxPooling(poolsize[n],1,poolstep[n],1))
-      	CNN_1L:add(nn.SpatialDropout(0.3)) -- dropout
+      	CNN_1L:add(nn.SpatialDropout(dropout1)) -- dropout
       
       	-- stage 2: linear -> ReLU -> linear
       	CNN_1L:add(nn.Reshape(ninputFC))
       	CNN_1L:add(nn.Linear(ninputFC,nstate_FC[n]))
       	if opt.batchNormalize == 'Yes' then CNN_1L:add(nn.BatchNormalization(nstate_FC[n])) end
       	CNN_1L:add(nn.ReLU())
-      	CNN_1L:add(nn.Dropout(0.8)) -- dropout
+	--if opt.batchNormalize == 'Yes' then CNN_1L:add(nn.BatchNormalization(nstate_FC[n])) end
+      	CNN_1L:add(nn.Dropout(dropout2)) -- dropout
 
       	if opt.typeMF == 'LS-Joint-LS' or opt.typeMF == 'LS-Add' or opt.typeMF == 'S-Add-L' or opt.typeMF == 'Add-LS' or opt.typeMF == 'Add-S' then
       		CNN_1L:add(nn.Linear(nstate_FC[n],noutputs)) -- output layer (output: 101 prediction probability)
